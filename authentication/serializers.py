0a7d0f01489178e55ae87157d4ea445bff5fc86e
Assem Ashraf <assemabudeif@gmail.com>
2024-09-14 21:43:17 +0300

    chore: finish login and register views

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
 from authentication.models import Client, Driver, BaseUser
-from core.models import City, PhoneNumber, Car, DriverLicense, NationalityID, UserType
+from core.models import City, PhoneNumber, Car, DriverLicense, NationalityID, UserType, Government
+from core.serializers import CitySerializer, PhoneNumberSerializer, CarSerializer, DriverLicenseSerializer, \
+    NationalityIDSerializer
 
 
 class BaseUserSerializer(serializers.ModelSerializer):
@@ -23,8 +25,10 @@ class DriverSerializer(serializers.ModelSerializer):
 
 
 class ClientRegisterSerializer(serializers.ModelSerializer):
-    city_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True)
-    phone_numbers = serializers.ListField(child=serializers.CharField(), write_only=True)
+    # city_ids = serializers.ListField(child=CitySerializer(), write_only=True)
+    city_ids = CitySerializer(many=True, write_only=True)  # Accepting nested City objects
+
+    phone_numbers = serializers.ListField(child=PhoneNumberSerializer(), write_only=True)
 
     class Meta:
         model = Client
@@ -46,9 +50,17 @@ class ClientRegisterSerializer(serializers.ModelSerializer):
             address_line=validated_data['address_line'],
             birth_date=validated_data['birth_date'],
         )
-        # Add cities
-        cities = City.objects.filter(id__in=city_ids)
-        user.city_ids.set(cities)
+
+        # Add or create cities
+        for city_data in city_ids:
+            government, created = Government.objects.get_or_create(
+                name=city_data['government']['name'],
+            )
+            city, created = City.objects.get_or_create(
+                name=city_data['name'],
+                government=government,
+            )
+            user.city_ids.add(city)
 
         # Add phone numbers (create new ones if they don't exist)
         for number in phone_numbers:
@@ -60,10 +72,11 @@ class ClientRegisterSerializer(serializers.ModelSerializer):
 
 
 class DriverRegisterSerializer(serializers.ModelSerializer):
-    city_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True)
-    phone_numbers = serializers.ListField(child=serializers.CharField(), write_only=True)
-    car_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True)
-    driver_license_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    city_ids = CitySerializer(many=True, write_only=True)  # Accepting nested City objects
+    phone_numbers = serializers.ListField(child=PhoneNumberSerializer(), write_only=True)
+    car_ids = serializers.ListField(child=CarSerializer(), write_only=True)
+    driver_license_ids = serializers.ListField(child=DriverLicenseSerializer(), write_only=True)
+    nationality_id = NationalityIDSerializer(write_only=True)
 
     class Meta:
         model = Driver
@@ -86,25 +99,57 @@ class DriverRegisterSerializer(serializers.ModelSerializer):
             name=validated_data['name'],
             address_line=validated_data['address_line'],
             birth_date=validated_data['birth_date'],
-            nationality_id=NationalityID.objects.get(id=validated_data['nationality_id']),
         )
 
-        # Add cities
-        cities = City.objects.filter(id__in=city_ids)
-        user.city_id.set(cities)
+        # Add or create cities
+        for city_data in city_ids:
+            government, created = Government.objects.get_or_create(
+                name=city_data['government']['name'],
+            )
+            city, created = City.objects.get_or_create(
+                name=city_data['name'],
+                government=government,
+            )
+            user.city_ids.add(city)
 
         # Add phone numbers (create new ones if they don't exist)
         for number in phone_numbers:
-            phone, created = PhoneNumber.objects.get_or_create(number=number)
+            phone, created = PhoneNumber.objects.get_or_create(phone_number=number['phone_number'])
             user.phone_numbers.add(phone)
 
         # Add cars
-        cars = Car.objects.filter(id__in=car_ids)
-        user.car_id.set(cars)
+        for car_data in car_ids:
+            car, created = Car.objects.get_or_create(
+                make=car_data['make'],
+                model=car_data['model'],
+                year=car_data['year'],
+            )
+            user.car_ids.add(car)
 
         # Add driver licenses
-        licenses = DriverLicense.objects.filter(id__in=driver_license_ids)
-        user.driver_license_id.set(licenses)
+        for license_data in driver_license_ids:
+            driver_license, created = DriverLicense.objects.get_or_create(
+                license_number=license_data['license_number'],
+                issued_date=license_data['issued_date'],
+                front_image_url=license_data['front_image_url'],
+                back_image_url=license_data['back_image_url'],
+                expiration_date=license_data['expiration_date'],
+            )
+            user.driver_license_ids.add(driver_license)
+
+        # Add nationality id
+        nationality_id = validated_data.pop('nationality_id', None)
+        if nationality_id:
+            user.nationality_id = NationalityID.objects.create(
+                nationality_id_number=nationality_id['nationality_id_number'],
+                issued_date=nationality_id['issued_date'],
+                expiration_date=nationality_id['expiration_date'],
+                front_image_url=nationality_id['front_image_url'],
+                back_image_url=nationality_id['back_image_url'],
+
+            )
 
         user.save()
         return user
+
+
